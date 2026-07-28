f6085d03e3fcc91f491250034dcb691130b401a1
Edwin Krasnokutsky <edwinkras29@gmail.com>
2026-07-28 18:32:31 -0400

    Add button Counter + reset button + TextInput into the Home Screen

--- wanderlist/src/app/index.tsx
+++ wanderlist/src/app/index.tsx
@@ -1,11 +1,18 @@
 import PlaceCard from "@/components/PlaceCard";
-import { ScrollView, Text, View, Button, StyleSheet, Image } from "react-native";
+import { ScrollView, Text, View, Button, StyleSheet, Image, TextInput } from "react-native";
+import { useState } from "react"; // only way it worked
 
 export default function Index() {
+  // Variable
+  //let count = 0; // somehow we don't need it now I guess
+
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+
   return (
     // it works, I wouldn't know how to do it without being told how to set up the scroll feature
     <ScrollView contentContainerStyle={{ padding: 16, gap: 12 }}>
-      <Header/>
+      <Header />
       <PlaceCard name="Kyoto" category="City" notes="Temples in autumn" />
       <PlaceCard name="Banff" category="Nature" notes="Food" />
       <PlaceCard
@@ -13,12 +20,30 @@ export default function Index() {
         category="Food"
         notes="Pasteisi de nata tour"
       />
-      <Image source={require('wanderlist/assets/images/Portrait.cloud.webp')}/>
-      <Button // wow, this is really cool
+      <Image source={require('wanderlist/assets/images/Portrait.cloud.webp')} />
+
+      {/*Adda button here:) */}
+      {/* it works*/}
+      <Button
+        title={`Count: ${count}`}
+        onPress={() => {
+          setCount(count + 1)
+        }} />
+
+      {/* works well*/}
+      <Button
+        title="Reset"
+        onPress={() => setCount(0)} />
+      {/* Title = count var, event counter++ */}
+
+      <TextInput value = {name} 
+      onChangeText={setName}
+      placeholder="Hello text input : ) "/>
+
+      {/* <Button // wow, this is really cool
         title="Press Me"
         onPress={() => window.alert('Simple Button pressed')}
-      // didn't think an actual window would open
-      />
+      // didn't think an actual window would open */}
     </ScrollView>
   )
   // the image worked
